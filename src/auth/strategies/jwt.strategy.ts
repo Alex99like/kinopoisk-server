c2d5f4ad5c09f1af3a117f12701761380d7822d0
Alexander <aleksievisa@gmail.com>
2023-01-23 08:50:53 +0300

    fix guard

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,26 +1,27 @@
-import { PassportStrategy } from "@nestjs/passport";
-import { ExtractJwt, Strategy } from "passport-jwt";
-import { ConfigService } from "@nestjs/config";
-import { InjectModel } from "nestjs-typegoose";
-import { UserModel } from "../../user/user.model";
-import { ModelType } from "@typegoose/typegoose/lib/types";
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ModelType } from '@typegoose/typegoose/lib/types'
+import { InjectModel } from 'nestjs-typegoose'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { UserService } from 'src/user/user.service'
+import { UserModel } from '../../user/user.model'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private readonly configService: ConfigService,
-    @InjectModel(UserModel)
-    private readonly UserModel: ModelType<UserModel>
-  ) {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
-      ignoreExpiration: true,
-      secretOrKey: configService.get('JWT_SECRET')
-    })
-  }
+	constructor(
+		private readonly configService: ConfigService,
+		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+	) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: true,
+			secretOrKey: configService.get('JWT_SECRET'),
+		})
+	}
 
-  async validate({ _id }: Pick<UserModel, '_id'>) {
-    return await this.UserModel.findById(_id).exec()
-  }
+	async validate({ _id }: Pick<UserModel, '_id'>) {
+		const user = await this.UserModel.findById(_id)
+		return user
+	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from "nestjs-typegoose";
+import { UserModel } from "./user.model";
+import { ModelType } from "@typegoose/typegoose/lib/types";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectModel(UserModel)
+    private readonly userModel: ModelType<UserModel>
+  ) {}
+
+  async byId() {
+    return { message: 'message' }
+  }
+}
